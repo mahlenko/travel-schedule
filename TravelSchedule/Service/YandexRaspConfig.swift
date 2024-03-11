72b2549ef0d299c80c9b51ac0596f06373c985da
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2024-03-11 14:22:20 +0300

    Create YandexRaspConfig

---
+++ TravelSchedule/Service/YandexRaspConfig.swift
@@ -0,0 +1,12 @@
+//
+//  YandexRaspConfigg.swift
+//  TravelSchedule
+//
+//  Created by Сергей Махленко on 10.03.2024.
+//
+
+import Foundation
+
+enum YandexRaspConfig {
+    static let APIKEY: String = ""
+}
